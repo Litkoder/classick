31e20a76dade418bf090ed9b4875743b7bba72ea
Litkoder <38402455+Litkoder@users.noreply.github.com>
2018-04-20 00:51:01 +0800

    Update ED.java
    设置了输入输出框,且输出框不可以编辑

--- ED.java
+++ ED.java
@@ -11,36 +11,54 @@ import java.awt.Font;
 
 public class ED extends JFrame {
 
-	private JTextArea ta;
+	private JTextArea input;
+	private JTextArea output;
 	private JButton jiami;
 	private JButton jiemi;
+	private JButton clear;
 
 	public ED() {
 		init();
 		southPanel();
 		centerPanel();
 		northPanel();
-		
 
 	
 	}
 	public void southPanel() {
-		JPanel south = new JPanel();			//创建南边的Panel
+		JPanel south = new JPanel();							//创建南边的Panel
 		jiami = new JButton("加 密");
 		jiemi = new JButton("解 密");
+		clear = new JButton("清 屏");
 		south.add(jiami);
 		south.add(jiemi);
+		south.add(clear);
 		this.add(south,BorderLayout.SOUTH);		//将Panel放在Frame的南边
 	}
 	public void northPanel() {
-		JPanel north = new JPanel();
-		JLabel ed = new JLabel ("请输入明文/密文:");
-		ta = new JTextArea(11,40);
-		north.add(ed);
-		north.add(ta);
-		ed.setFont(new Font("1",Font.PLAIN,13));
-		ta.setFont(new Font("1",Font.PLAIN,13));
-		this.add(north,BorderLayout.NORTH);
+		JPanel north1 = new JPanel();
+		JLabel source = new JLabel ("请输入明文/密文:");
+		input = new JTextArea(7,40);
+		north1.add(source);
+		north1.add(input);
+		source.setFont(new Font("1",Font.PLAIN,13));
+		input.setFont(new Font("1",Font.PLAIN,13));
+
+		JPanel north2 = new JPanel();
+		JLabel result = new JLabel ("输出的明文/密文:");
+		output = new JTextArea(7,40);
+		north2.add(result);
+		north2.add(output);
+		result.setFont(new Font("1",Font.PLAIN,13));
+		output.setFont(new Font("1",Font.PLAIN,13));
+		output.setEditable(false);
+
+		JPanel panelnorth = new JPanel();					// 画板采用边界布局管理器
+        panelnorth.setLayout(new BorderLayout());
+        panelnorth.add("North", north1);
+        panelnorth.add("Center", north2);
+		this.add(panelnorth,BorderLayout.NORTH);
+		
 
 	}
 	public void centerPanel() {
