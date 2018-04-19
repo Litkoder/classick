8d2b960c02b24b202f39863343891591c88ce145
Litkoder <38402455+Litkoder@users.noreply.github.com>
2018-04-20 07:31:15 +0800

    Update ED.java
    增加了密钥的选择及监听

--- ED.java
+++ ED.java
@@ -6,20 +6,22 @@ import javax.swing.JTextField;
 import javax.swing.JTextArea;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.JOptionPane;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
-
 
 
 public class ED extends JFrame {
 
 	private JTextArea input;
 	private JTextArea output;
+	private JTextField key1;
+	private JTextField key2;
 	private JButton jiami;
 	private JButton jiemi;
 	private JButton clear;
-
+	
 	public ED() {
 		init();
 		southPanel();
@@ -27,7 +29,6 @@ public class ED extends JFrame {
 		northPanel();
 		event();
 
-	
 	}
 	public void event() {
 		jiami.addActionListener(new ActionListener () {
@@ -49,31 +50,72 @@ public class ED extends JFrame {
 		
 	}
 	private void jiami() {
-			String message = input.getText();	//获取输入区的内容
-			//int kkey = key1.getText();
-			//int zkey = key2.getText();
+			String message = input.getText();			//获取输入区的内容
+			String mykey1=key1.getText();				//获取key1的内容
+			String mykey2=key2.getText();				//获取key2的内容
 
-			output.setText(message);		//放message做测试
+			if(message == null || message.equals("")) {
+				JOptionPane.showMessageDialog(null, "输入的明文/密文不能为空!");    
+				input.requestFocus();
+			} else {
+
+				if((mykey1 == null || mykey1.equals("")) && (mykey2 == null || mykey2.equals(""))) {
+					JOptionPane.showMessageDialog(null, "密钥不能为空!");    
+					key1.requestFocus();
+				} else if (mykey1 != null && (mykey2 == null || mykey2.equals(""))) {
+					JOptionPane.showMessageDialog(null, "测试1!");    
+					
+				} else if((mykey1 == null || mykey1.equals("")) && mykey2 != null) {
+					JOptionPane.showMessageDialog(null, "测试2!");
+					
+				} else {
+					JOptionPane.showMessageDialog(null, "密钥只能同时输入一个!");
+					key1.requestFocus();
+				}
+				
+			}
+			output.setText(message);				//放message测试
 
 		}
 		private void jiemi() {
-			String message = input.getText();	//获取输入区的内容
-			//int kkey = key1.getText();
-			//int zkey = key2.getText();
+			String message = input.getText();			//获取输入区的内容
+			String mykey1=key1.getText();				//获取key1的内容
+			String mykey2=key2.getText();				//获取key2的内容
 
-			output.setText(message);
+			if(message == null || message.equals("")) {
+				JOptionPane.showMessageDialog(null, "输入的明文/密文不能为空!");    
+				input.requestFocus();
+			} else {
+
+				if((mykey1 == null || mykey1.equals("")) && (mykey2 == null || mykey2.equals(""))) {
+					JOptionPane.showMessageDialog(null, "密钥不能为空!");    
+					key1.requestFocus();
+				} else if (mykey1 != null && (mykey2 == null || mykey2.equals(""))) {
+					JOptionPane.showMessageDialog(null, "测试1!");    
+					
+				} else if((mykey1 == null || mykey1.equals("")) && mykey2 != null) {
+					JOptionPane.showMessageDialog(null, "测试2!");
+					
+				} else {
+					JOptionPane.showMessageDialog(null, "密钥只能同时输入一个!");
+					key1.requestFocus();
+				}
+				
+			}
+
+			output.setText(message);				//放message测试
 
 		}
 
 	public void southPanel() {
-		JPanel south = new JPanel();			//创建南边的Panel
+		JPanel south = new JPanel();					//创建南边的Panel
 		jiami = new JButton("加 密");
 		jiemi = new JButton("解 密");
 		clear = new JButton("清 屏");
 		south.add(jiami);
 		south.add(jiemi);
 		south.add(clear);
-		this.add(south,BorderLayout.SOUTH);		//将Panel放在Frame的南边
+		this.add(south,BorderLayout.SOUTH);				//将Panel放在Frame的南边
 	}
 	public void northPanel() {
 		JPanel north1 = new JPanel();
@@ -93,7 +135,7 @@ public class ED extends JFrame {
 		output.setFont(new Font("1",Font.PLAIN,13));
 		output.setEditable(false);
 
-		JPanel panelnorth = new JPanel();		// 画板采用边界布局管理器
+		JPanel panelnorth = new JPanel();				// 画板采用边界布局管理器
         panelnorth.setLayout(new BorderLayout());
         panelnorth.add("North", north1);
         panelnorth.add("Center", north2);
@@ -103,9 +145,9 @@ public class ED extends JFrame {
 	public void centerPanel() {
 		JPanel center = new JPanel();
 		JLabel kaisa = new JLabel ("凯撒密码密钥:");
-		JTextField key1 = new JTextField(3);
+		key1 = new JTextField(3);
 		JLabel zhalan = new JLabel ("栅栏密码密钥:");
-		JTextField key2 = new JTextField(3);
+		key2 = new JTextField(3);
 		center.add(kaisa);
 		center.add(key1);
 		center.add(zhalan);
@@ -122,8 +164,6 @@ public class ED extends JFrame {
 		this.setSize(600,400);
 		this.setVisible(true);
 	}
-
-
 
 
 	public static void main(String[] args) {
