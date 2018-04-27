fecf306bc68383c01241ba20da283c3e45b27756
Litkoder <38402455+Litkoder@users.noreply.github.com>
2018-04-27 18:03:23 +0800

    Update ED.java
    增加凯撒密码加密解密功能

--- ED.java
+++ ED.java
@@ -1,12 +1,12 @@
 import javax.swing.JFrame;
-import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JTextArea;
 import javax.swing.JLabel;
-import java.awt.Font;
 import javax.swing.JOptionPane;
+import java.awt.BorderLayout;
+import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
@@ -15,7 +15,6 @@ import java.net.UnknownHostException;
 
 
 public class ED extends JFrame {
-
 	private JTextArea input;
 	private JTextArea output;
 	private JTextField key1;
@@ -30,8 +29,8 @@ public class ED extends JFrame {
 		centerPanel();
 		northPanel();
 		event();
-
 	}
+
 	public void event() {
 		jiami.addActionListener(new ActionListener () {
 			public void actionPerformed(ActionEvent e) {
@@ -39,10 +38,8 @@ public class ED extends JFrame {
 					jiami();
 				} catch (IOException e1) {
 					e1.printStackTrace();
-				}
-			
+				}			
 			}
-
 		});
 		jiemi.addActionListener(new ActionListener () {
 			public void actionPerformed(ActionEvent e) {
@@ -50,74 +47,122 @@ public class ED extends JFrame {
 					jiemi();
 				} catch (IOException e1) {
 					e1.printStackTrace();
-				}
-				
-
+				}				
 			}
-
-		});
-		
+		});		
 	}
+
 	private void jiami() throws IOException {
-			String message = input.getText();			//获取输入区的内容
-			String mykey1=key1.getText();				//获取key1的内容
-			String mykey2=key2.getText();				//获取key2的内容
+		String message = input.getText();			//获取输入区的内容
+		String mykey1=key1.getText();				//获取key1的内容
+		String mykey2=key2.getText();				//获取key2的内容
 
-			if(message == null || message.equals("")) {
-				JOptionPane.showMessageDialog(null, "输入的明文/密文不能为空!");    
-				input.requestFocus();
-			} else {
+		if(message == null || message.equals("")) {
+			JOptionPane.showMessageDialog(null, "输入的明文/密文不能为空!");    
+			input.requestFocus();
+		} else {
 
-				if((mykey1 == null || mykey1.equals("")) && (mykey2 == null || mykey2.equals(""))) {
-					JOptionPane.showMessageDialog(null, "密钥不能为空!");    
-					key1.requestFocus();
-				} else if (mykey1 != null && (mykey2 == null || mykey2.equals(""))) {
-					JOptionPane.showMessageDialog(null, "测试1!");    
+			if((mykey1 == null || mykey1.equals("")) && (mykey2 == null || mykey2.equals(""))) {
+				JOptionPane.showMessageDialog(null, "密钥不能为空!");    
+				key1.requestFocus();
+			} else if (mykey1 != null && (mykey2 == null || mykey2.equals(""))) {
+				JOptionPane.showMessageDialog(null, "测试1!"); 
+				encrypt(message,mykey1);		
 					
-				} else if((mykey1 == null || mykey1.equals("")) && mykey2 != null) {
-					JOptionPane.showMessageDialog(null, "测试2!");
-					
-				} else {
-					JOptionPane.showMessageDialog(null, "密钥只能同时输入一个!");
-					key1.requestFocus();
-				}
+			} else if((mykey1 == null || mykey1.equals("")) && mykey2 != null) {
+				JOptionPane.showMessageDialog(null, "测试2!");
 				
-			}
-			output.setText(message);							//放message测试
-
-		}
-		private void jiemi() throws IOException {
-			String message = input.getText();			//获取输入区的内容
-			String mykey1=key1.getText();				//获取key1的内容
-			String mykey2=key2.getText();				//获取key2的内容
-
-			if(message == null || message.equals("")) {
-				JOptionPane.showMessageDialog(null, "输入的明文/密文不能为空!");    
-				input.requestFocus();
+					
 			} else {
-
-				if((mykey1 == null || mykey1.equals("")) && (mykey2 == null || mykey2.equals(""))) {
-					JOptionPane.showMessageDialog(null, "密钥不能为空!");    
-					key1.requestFocus();
-				} else if (mykey1 != null && (mykey2 == null || mykey2.equals(""))) {
-					JOptionPane.showMessageDialog(null, "测试1!");    
-					
-				} else if((mykey1 == null || mykey1.equals("")) && mykey2 != null) {
-					JOptionPane.showMessageDialog(null, "测试2!");
-					
-				} else {
-					JOptionPane.showMessageDialog(null, "密钥只能同时输入一个!");
-					key1.requestFocus();
-				}
-				
-			}
-
-			output.setText(message);							//放message测试
-
+				JOptionPane.showMessageDialog(null, "密钥只能同时输入一个!");
+				key1.requestFocus();
+			}			
 		}
+	}
 
+	private void jiemi() throws IOException {
+		String message = input.getText();			//获取输入区的内容
+		String mykey1=key1.getText();				//获取key1的内容
+		String mykey2=key2.getText();				//获取key2的内容
+
+		if(message == null || message.equals("")) {
+			JOptionPane.showMessageDialog(null, "输入的明文/密文不能为空!");    
+			input.requestFocus();
+		} else {
+
+			if((mykey1 == null || mykey1.equals("")) && (mykey2 == null || mykey2.equals(""))) {
+				JOptionPane.showMessageDialog(null, "密钥不能为空!");    
+				key1.requestFocus();
+			} else if (mykey1 != null && (mykey2 == null || mykey2.equals(""))) {
+				JOptionPane.showMessageDialog(null, "测试1!");    
+				decrypt(message,mykey1);
+
+					
+			} else if((mykey1 == null || mykey1.equals("")) && mykey2 != null) {
+				JOptionPane.showMessageDialog(null, "测试2!");
+					
+			} else {
+				JOptionPane.showMessageDialog(null, "密钥只能同时输入一个!");
+				key1.requestFocus();
+			}			
+		}
+	}
+
+	public String encrypt(String str, String key) {
+		StringBuilder result = new StringBuilder();
+		int k = Integer.parseInt(key);
+		for (char c : str.toCharArray()) {					
+			if (c >= 'a' && c <= 'z') {		// 如果字符串中的某个字符是小写字母
+				c += k % 26;			// 移动key%26位
+					if (c < 'a')
+						c += 26;											// 向左超界
+					if (c > 'z')
+						c -= 26;											// 向右超界
+			}																				
+			else if (c >= 'A' && c <= 'Z') {	// 如果字符串中的某个字符是大写字母
+				c += k % 26;			// 移动key%26位
+				if (c < 'A')
+					c += 26;												// 向左超界
+				if (c > 'Z')
+					c -= 26;												// 向右超界
+			}
+			result.append(c);
+		}
+		output.setText(result.toString());
+		return result.toString();	
+	}
+
+	public String decrypt(String str, String key) {
+		StringBuilder result = new StringBuilder();
+		int k = Integer.parseInt(key);
+		k = 0 - k;
+		for (char c : str.toCharArray()) {					
+			if (c >= 'a' && c <= 'z') {		// 如果字符串中的某个字符是小写字母
+				c += k % 26;			// 移动key%26位
+					if (c < 'a') {		// 向左超界
+						c += 26;
+					}
+					if (c > 'z') {		// 向右超界
+						c -= 26;
+					}
+			}																		
+			else if (c >= 'A' && c <= 'Z') {	// 如果字符串中的某个字符是大写字母
+				c += k % 26;			// 移动key%26位
+				if (c < 'A') {			// 向左超界
+					c += 26;
+				}
+				if (c > 'Z') {			// 向右超界
+					c -= 26;
+				}
+			}
+			result.append(c);
+		}
+		output.setText(result.toString());
+		return result.toString();
+	}
+	
 	public void southPanel() {
-		JPanel south = new JPanel();							//创建南边的Panel
+		JPanel south = new JPanel();			//创建南边的Panel
 		jiami = new JButton("加 密");
 		jiemi = new JButton("解 密");
 		clear = new JButton("清 屏");
@@ -144,7 +189,7 @@ public class ED extends JFrame {
 		output.setFont(new Font("1",Font.PLAIN,13));
 		output.setEditable(false);
 
-		JPanel panelnorth = new JPanel();					// 画板采用边界布局管理器
+		JPanel panelnorth = new JPanel();		// 画板采用边界布局管理器
         panelnorth.setLayout(new BorderLayout());
         panelnorth.add("North", north1);
         panelnorth.add("Center", north2);
